c3eb90a5dc24f7cb849100345f081d4a18b8d976
Ashutosh Verma <vkrypto@gmail.com>
2024-11-26 00:18:04 +0530

    file logging + console logging is working.. perfectly..

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -2,16 +2,23 @@ import logging
 import logging.config
 import json
 from pathlib import Path
-import graypy  # noqa
+import graypy   # :noqa
+import os
 
 
+cur_dir = Path(__file__).parent
+log_dir = cur_dir.parent.joinpath("logs")
 
-config_file = Path(__file__).parent.joinpath("./config.json")
+if not log_dir.exists():
+    os.makedirs(log_dir)
+
+config_file = cur_dir.joinpath("./config.json")
 if not config_file.exists():
     raise FileNotFoundError(f"Configuration file not found: {config_file}")
-
 with config_file.open("r") as f:
     config = json.load(f)
-
+    
+    
+config["handlers"]["file_handler"]["filename"] = log_dir.joinpath("logs.log")
 logging.config.dictConfig(config)
 logger = logging.getLogger(__name__)
